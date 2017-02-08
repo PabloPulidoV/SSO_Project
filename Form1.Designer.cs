be5b4e68e7bed4168abe445e99449ecf12e21829
Pablo Pulido Vallejo <pablopulidovl11@gmail.com>
2017-02-08 16:07:35 -0600

    Primer Commit
    Diseño sin funcionalidades

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Sem_SO_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombProg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Op = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.NombProg,
+            this.Op,
+            this.TME});
+            this.dataGridView1.Location = new System.Drawing.Point(64, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(445, 242);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // NombProg
+            // 
+            this.NombProg.HeaderText = "Developer Name";
+            this.NombProg.Name = "NombProg";
+            // 
+            // Op
+            // 
+            this.Op.HeaderText = "Operation";
+            this.Op.Name = "Op";
+            // 
+            // TME
+            // 
+            this.TME.HeaderText = "Tiempo Máximo";
+            this.TME.Name = "TME";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(406, 338);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(103, 33);
+            this.start.TabIndex = 1;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 394);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombProg;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Op;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TME;
+        private System.Windows.Forms.Button start;
+    }
+}
+
